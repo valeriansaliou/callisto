83e0f3a29946f808238cd4caead9be908c455dce
Valerian Saliou <valerian@valeriansaliou.name>
2016-04-27 15:04:05 +0200

    Proper Saturn Rings
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- circle.go
+++ circle.go
@@ -152,8 +152,8 @@ func generateCircleObject(circle *Circle, radius_n float32, thickness float32, a
   }
 
   // Bind circle texture coordinates
-  circle.TextureCoords[*l] = float32(angle / float64(angle_max))
-  circle.TextureCoords[*l + 1] = float32(angle / float64(angle_max))
+  circle.TextureCoords[*l] = float32(pass_index)
+  circle.TextureCoords[*l + 1] = 0.0
 
   *l += 2
 }
